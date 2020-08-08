8e034a0a7fa8403d8550b6dd1bedc18b9b35bc4f
Julien <Juknum@users.noreply.github.com>
2020-08-08 22:46:22 +0200

    fix non square of 2 images (fix wrong render)

--- js/hideElement.js
+++ js/hideElement.js
@@ -7,11 +7,11 @@
 */
 
 function hideElement(id) {
-		var x = document.getElementById(id);
-		if (x.style.display === "none") {
-			x.style.display = "block";
-		}
-		else{
-			x.style.display = "none";
-		}
+	var x = document.getElementById(id);
+	if (x.style.display === "none") {
+		x.style.display = "block";
 	}
+	else{
+		x.style.display = "none";
+	}
+}
