d8f5a709cc7ea861e919c8006a23686c379de6fb
Julien <Juknum@users.noreply.github.com>
2020-08-08 16:07:07 +0200

    move js to his own file

---
+++ js/hideElement.js
@@ -0,0 +1,17 @@
+// Hide an element if image url is broken
+/*
+* Usage :
+*		<div class="col" id="Element-to-hide">
+*			<img href="#" onerror=hideElement(Element-to-hide) />
+*		</div>
+*/
+
+function hideElement(id) {
+		var x = document.getElementById(id);
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		}
+		else{
+			x.style.display = "none";
+		}
+	}
