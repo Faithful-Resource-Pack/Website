5dddae683050b20cd672593602e2cf64be33d87a
Julien <Juknum@users.noreply.github.com>
2020-08-09 23:36:37 +0200

    fixed close button

--- js/showModal.js
+++ js/showModal.js
@@ -12,12 +12,4 @@ function showModal(ModalID, ImgID, ModalImgID, TextureName){
 	  modalImg.src = this.src;
 	  captionText.innerHTML = this.alt;
 	}
-
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() { 
-	  modal.style.display = "none";
-	}
 }
