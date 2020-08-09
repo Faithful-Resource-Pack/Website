2bf9e56424a3c8c1bf8f364bd686601cb842f479
Julien <Juknum@users.noreply.github.com>
2020-08-09 23:17:59 +0200

    replace pop out effect with modal

---
+++ js/showModal.js
@@ -0,0 +1,23 @@
+
+function showModal(ModalID, ImgID, ModalImgID, TextureName){
+	var modal = document.getElementById(ModalID);
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	var img = document.getElementById(ImgID);
+	var modalImg = document.getElementById(ModalImgID);
+	var captionText = document.getElementById(TextureName);
+
+	img.onclick = function(){
+	  modal.style.display = "block";
+	  modalImg.src = this.src;
+	  captionText.innerHTML = this.alt;
+	}
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+	  modal.style.display = "none";
+	}
+}
