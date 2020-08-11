6dc5059f696cfb742f32f2e64e5d0adb09e5e2e3
Robert Reichel <robertreich1212@gmail.com>
2020-08-11 21:35:06 +0200

    add closing when clicking on background back again

--- js/showModal.js
+++ js/showModal.js
@@ -39,7 +39,7 @@ function showModal(ModalID, ImgID, ModalImgID, TextureName, SpanID){
 	span.onclick = function(){
 		modal.style.display = "none"
 	}
-	/*$('.modal').click(function()){
+	$('.modal-background').click(function()){
 		modal.style.display = "none"
-	}*/
+	}
 }
