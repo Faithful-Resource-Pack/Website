e1fb0329a1667a097c6efb369b23f68084310110
Julien <Juknum@users.noreply.github.com>
2020-08-31 14:48:59 +0200

    add date

--- js/ShowInfos.js
+++ js/ShowInfos.js
@@ -31,15 +31,18 @@ function ShowInfos(imgURL, captionText, blockTexture){
 		url += '/UE4Project/Content/' + captionText + '.png';
 	}
 
-	var auth = getJSON(url, function(err, data){
+	getJSON(url, function(err, data){
 		if (err !== null){
 			console.log('Something went wrong: ' + err);
 			var authTxt = 'GitHub API error:' + err;
 		} else {
+
+			// authors:
 			var count = 0;
 			const MAX_COUNT = 20;
 			var authArr = new Array();
 			var authTxt = '';
+
 			while (data[count] !== undefined || count < MAX_COUNT){ // while data isn't broken or count is reached (max 100)
 				//console.log(data[count]);
 				if(data[count]){
@@ -57,19 +60,23 @@ function ShowInfos(imgURL, captionText, blockTexture){
 					authTxt = authArr;
 				}	
 			}
+
+			// date: (last commit)
+			var dateTxt = data[0].commit.author.date;
 		}
 		authTxt = authTxt.toString().replace(/,/g, ", ");
 		document.getElementById("auth").innerHTML = '<strong>Authors:</strong> ' + authTxt;
+
+		dateTxt = sizeTxt.toString().substring(0, 10).replace(/-/g, '/');
+		document.getElementById("date").innerHTML = '<strong>Published:</strong> ' + date;
 	});
 
 	// WIP
 	// call github api to get infos about the texture:
 	var size = 'size'; // dimensions (32x32, 64x64, ...)
-	var date = 'date'; 	
 	var uses = ['use1','use2','use3']; // use json (use for blocks textures (result: squidcoast, ...))
 	
 	document.getElementById("size").innerHTML = '<strong>Size:</strong> ' + size;
-	document.getElementById("date").innerHTML = '<strong>Published:</strong> ' + date;
 	document.getElementById("uses").innerHTML = '<strong>Used in:</strong> ' + uses;
 
 	// Set img
