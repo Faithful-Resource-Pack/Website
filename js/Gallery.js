8249fc76effccc5d1d537510999428ecabab057e
Julien <Juknum@users.noreply.github.com>
2020-11-16 17:07:15 +0100

    improve gallery & remove mojang textures

--- js/Gallery.js
+++ js/Gallery.js
@@ -5,8 +5,8 @@ function Hide(id){
 	col2 = document.getElementById('COL2-'+id);
 	col3 = document.getElementById('COL3-'+id);
 
-	col1.innerHTML = '<h2 class="display-5 m-3 text-center">16x</h2>';
-	col2.innerHTML = '<h2 class="display-5 m-3 text-center">32x</h2>';
+	col1.innerHTML = '<h2 class="display-5 m-3 text-center">32x</h2>';
+	col2.innerHTML = '<h2 class="display-5 m-3 text-center">Texture Name</h2>';
 	col3.innerHTML = '<h2 class="display-5 m-3 text-center">64x</h2>';
 
 	col1.style.display = 'none';
@@ -14,6 +14,29 @@ function Hide(id){
 	col3.style.display = 'none';
 
 	btn.style.display = 'none';
+}
+
+function Fullscreen(file) {
+	var modal      = document.getElementById('FULLSCREEN');
+	var modal_img  = document.getElementById('FULLSCREEN-IMG');
+	var modal_span = document.getElementById('FULLSCREEN-SPAN');
+
+	modal_img.src = file;
+	modal.style.display = 'block';
+
+	//
+	// Close modal:
+	// 
+	modal_span.onclick = function() {
+		modal.style.display = 'none';
+	};
+	// By pressing escape:
+	$(document).keydown(function(event) {
+		if (event.keyCode == 27) {
+			modal.style.display = 'none'
+		}
+	});
+
 }
 
 function Load_Textures(file,done) {
@@ -68,9 +91,43 @@ function Load_Textures(file,done) {
 
 			if (k < MAX && k < json.length && MC_TYPE == 'JAVA-RESULT'){
 				console.log(k,MAX)
-				col1.innerHTML += `<img alt="Not done yet" src="https://raw.githubusercontent.com/Compliance-Resource-Pack/Default-texture/master/Java/` + VERSION + json[k] + `" width="` + SIZE + `" height="` + SIZE + `" class="center img-gallery"><br>`
-				col2.innerHTML += `<img alt="Not done yet" src="https://raw.githubusercontent.com/Compliance-Resource-Pack/Resource-Pack-32x/master/Jappa/` + VERSION + `/assets` + json[k] + `" width="` + SIZE + `" height="` + SIZE + `" class="center img-gallery"><br>`
-				col3.innerHTML += `<img alt="Not done yet" src="https://raw.githubusercontent.com/Compliance-Resource-Pack/Resource-Pack-64x/master/Jappa/` + VERSION + `/assets` + json[k] + `" width="` + SIZE + `" height="` + SIZE + `" class="center img-gallery"><br>`
+				col1.innerHTML += 
+				`
+					<img class="center img-gallery" alt="Not done yet" src="https://raw.githubusercontent.com/Compliance-Resource-Pack/Resource-Pack-32x/master/Jappa/` + VERSION + `/assets` + json[k] + `" width="` + SIZE + `" height="` + SIZE + `">
+					<br>
+				`
+				col2.innerHTML += 
+				`
+					<div class="card card-sized">
+						<div class="card-body">
+							<h4 class="card-title text-center title-top">` + json[k].split("/").pop() + `</h4>
+							<h5 class="card-title text-center">See on GitHub:</h5>
+							<div class="row">
+								<div class="col">
+									<a target="_blank" class="btn btn-block btn-success" href="https://github.com/Compliance-Resource-Pack/Resource-Pack-32x/tree/master/Jappa/` + VERSION + `/assets` + json[k] + `">32x</a>
+								</div>
+								<div class="col">
+									<a target="_blank" class="btn btn-block btn-success" href="https://github.com/Compliance-Resource-Pack/Resource-Pack-64x/tree/master/Jappa/` + VERSION + `/assets` + json[k] + `">64x</a>
+								</div>
+							</div>
+							<h5 class="card-title text-center">Fullscreen:</h5>
+							<div class="row">
+								<div class="col">
+									<button class="btn btn-block btn-primary" onclick="Fullscreen('https://raw.githubusercontent.com/Compliance-Resource-Pack/Resource-Pack-32x/master/Jappa/` + VERSION + `/assets` + json[k] + `')">32x</button>
+								</div>
+								<div class="col">
+									<button class="btn btn-block btn-primary" onclick="Fullscreen('https://raw.githubusercontent.com/Compliance-Resource-Pack/Resource-Pack-64x/master/Jappa/` + VERSION + `/assets` + json[k] + `')">64x</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br>
+				`
+				col3.innerHTML += 
+				`
+					<img class="center img-gallery" alt="Not done yet" src="https://raw.githubusercontent.com/Compliance-Resource-Pack/Resource-Pack-64x/master/Jappa/` + VERSION + `/assets` + json[k] + `" width="` + SIZE + `" height="` + SIZE + `">
+					<br>
+				`
 			}
 		}
 
