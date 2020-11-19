456000bbf9a92251082651a1aa0c6b4b288dd804
Julien <Juknum@users.noreply.github.com>
2020-11-19 21:34:46 +0100

    fix bug when user come for the first time, adds icons

--- js/darktheme.js
+++ js/darktheme.js
@@ -2,11 +2,17 @@ const currentTheme = localStorage.getItem('theme');
 const css = document.getElementById('darkcss')
 const btn = document.getElementById('DarkMode')
 
+const text_off  = '<i style="margin-right: 5px" class="fas fa-sun"></i> Dark Mode'
+const text_auto = '<i style="margin-right: 5px" class="fas fa-adjust"></i> Dark Mode'
+const text_on   = '<i style="margin-right: 5px" class="fas fa-moon"></i> Dark Mode'
+
 // update btn
 if (currentTheme) {
-	if (currentTheme == 'dark') { btn.innerHTML = 'Dark Mode (ON)' }
-	if (currentTheme == 'dark-auto') { btn.innerHTML = 'Dark Mode (Auto)' }
-	if (currentTheme == 'light') { btn.innerHTML = 'Dark Mode (OFF)' }
+	if (currentTheme == 'dark') { btn.innerHTML = text_on }
+	if (currentTheme == 'dark-auto') { btn.innerHTML = text_auto }
+	if (currentTheme == 'light') { btn.innerHTML = text_off }
+} else {
+	localStorage.setItem('theme', 'dark-auto'); // if the user come for the first time
 }
 
 changeMod(false)
@@ -15,16 +21,16 @@ function changeMod(change) {
 
 	// true if the btn is it, false otherwise
 	if (change) {
-		if (btn.innerHTML == 'Dark Mode (Auto)') {
-			btn.innerHTML = 'Dark Mode (ON)';
+		if (btn.innerHTML == text_auto) {
+			btn.innerHTML = text_on;
 			localStorage.setItem('theme', 'dark');
 
-		} else if (btn.innerHTML == 'Dark Mode (ON)') {
-			btn.innerHTML = 'Dark Mode (OFF)';
+		} else if (btn.innerHTML == text_on) {
+			btn.innerHTML = text_off;
 			localStorage.setItem('theme', 'light');
 
-		} else if (btn.innerHTML == 'Dark Mode (OFF)') {
-			btn.innerHTML = 'Dark Mode (Auto)';
+		} else if (btn.innerHTML == text_off) {
+			btn.innerHTML = text_auto;
 			localStorage.setItem('theme', 'dark-auto');
 
 		}
@@ -35,6 +41,6 @@ function changeMod(change) {
 	if ( theme == 'dark' || (theme == 'dark-auto' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
 		css.href = "./css/dark.css"
 	} else { 
-		css.href = "light mode is on"
+		css.href = ""
 	}
 }
