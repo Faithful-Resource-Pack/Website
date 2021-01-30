f79fd3995ec458fe24fad7d1881295385ade8510
TheRolf <yann.lv@live.fr>
2021-01-30 10:59:06 +0100

    Fixed dropdown bug and simplified code

--- js/defaultFunctionality.js
+++ js/defaultFunctionality.js
@@ -1,71 +1,82 @@
+/* global Element */
+
+Element.prototype.siblingElements = function () {
+  if (this.parentNode === null) return []
+
+  return [...this.parentNode.children].filter(el => el !== this)
+}
+
+const DROPDOWN_SHOW_CLASS = 'show'
+
+// at initialization
 document.querySelectorAll('[data-toggle="dropdown"]').forEach(item => {
-  item.ariaExpanded  = false
+  // say that all collapsible element not expanded
+  item.ariaExpanded = false
+
+  // add click listener to toggle dropdowns
   item.addEventListener('click', () => {
     toggleDropdown(item)
   })
 })
 
-function toggleDropdown(item) {
-  let expandNow = true
-  if (item.dataset.expanded == 'true') expandNow = false
-
-  item.parentNode.classList.toggle('show', expandNow)
-  getNextElement(item, '.dropdown-menu').classList.toggle('show', expandNow)
-  item.dataset.expanded = expandNow
-
-  if (expandNow) {
-    setTimeout(() => {
-      window.addEventListener('click', event => {
-        if (!item.contains(event.target)) toggleDropdown(item)
-      }, { once: true })
-    }, 500)
+// function made to toggle my item and untoggle all others
+function toggleDropdown (item) {
+  if (!item.ariaExpanded) { // if I am not expanded, go untoggle all other siblings
+    item.parentNode.siblingElements().filter(el => el.classList.contains(DROPDOWN_SHOW_CLASS)).forEach(otherParent => { // you do not need to filter other with show class because it will be removed anyway
+      otherParent.querySelector('[data-toggle="dropdown"]').ariaExpanded = false
+      otherParent.classList.remove(DROPDOWN_SHOW_CLASS)
+    })
   }
+
+  // then toggle me
+  item.ariaExpanded = !item.ariaExpanded
+  item.parentNode.classList.toggle(DROPDOWN_SHOW_CLASS)
 }
 
 document.querySelectorAll('[data-toggle="collapse"]').forEach(item => {
-  item.dataset.expanded  = false
+  item.dataset.expanded = false
   item.addEventListener('click', () => {
     toggleMenu(item)
   })
 })
 
-function toggleMenu(item) {
+function toggleMenu (item) {
   let expandNow = true
-  if (item.dataset.expanded == 'true') expandNow = false
+  if (item.dataset.expanded === 'true') expandNow = false
 
   item.classList.toggle('collapsed', expandNow)
   getNextElement(item, item.dataset.target).classList.toggle('show', expandNow)
   item.dataset.expanded = expandNow
 }
 
-function getNextElement(element, selector) {
+function getNextElement (element, selector) {
   let next = element.nextElementSibling
   while (next) {
-		if (next.matches(selector)) return next
-		next = next.nextElementSibling
-	}
+    if (next.matches(selector)) return next
+    next = next.nextElementSibling
+  }
 }
 
 document.querySelectorAll('[data-ride="carousel"]').forEach(item => {
-  let indicators = item.querySelector('.carousel-indicators').children
-  let itemCount = indicators.length
+  const indicators = item.querySelector('.carousel-indicators').children
+  const itemCount = indicators.length
   let currentItem = 0
   let blocking = false
 
   item.querySelector('[data-slide="prev"]').addEventListener('click', e => {
     e.preventDefault()
-    if (currentItem == 0) goToSlide(item, itemCount - 1)
+    if (currentItem === 0) goToSlide(item, itemCount - 1)
     else goToSlide(item, currentItem - 1)
   })
 
   item.querySelector('[data-slide="next"]').addEventListener('click', e => {
     e.preventDefault()
-    if (currentItem == itemCount - 1) goToSlide(item, 0)
+    if (currentItem === itemCount - 1) goToSlide(item, 0)
     else goToSlide(item, currentItem + 1)
   })
 
-  setTimeout(function loop() {
-    if (currentItem == itemCount - 1) goToSlide(item, 0)
+  setTimeout(function loop () {
+    if (currentItem === itemCount - 1) goToSlide(item, 0)
     else goToSlide(item, currentItem + 1)
     setTimeout(loop, 5000)
   }, 5000)
@@ -76,12 +87,12 @@ document.querySelectorAll('[data-ride="carousel"]').forEach(item => {
     })
   }
 
-  function goToSlide(carousel, index) {
-    if (!blocking && index != currentItem) {
+  function goToSlide (carousel, index) {
+    if (!blocking && index !== currentItem) {
       blocking = true
 
-      let carouselIIndicators = carousel.querySelector('.carousel-indicators').children
-      let carouselItems = carousel.querySelector('.carousel-inner').children
+      const carouselIIndicators = carousel.querySelector('.carousel-indicators').children
+      const carouselItems = carousel.querySelector('.carousel-inner').children
 
       carouselIIndicators.item(currentItem).classList.remove('active')
       carouselIIndicators.item(index).classList.add('active')
