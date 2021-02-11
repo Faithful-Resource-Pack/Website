22b9e520528cad4fbdb050fcb169af4bb01575f0
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-02-11 13:38:10 +0100

    Add complete navigation

--- js/gallery/pages/gallery-page.js
+++ js/gallery/pages/gallery-page.js
@@ -3,8 +3,20 @@ export default {
   props: {
     version: String
   },
+  data() {
+    return {
+      versions: ['java-32x', 'java-64x', 'bedrock-32x', 'bedrock-64x', 'dungeons', 'education'],
+      sections: ['armor', 'blocks', 'colormaps', 'entities', 'environment', 'font', 'gui', 'items', 'map', 'mob-effects', 'paintings', 'particles', 'others']
+    }
+  },
   template:
   `<div>
+    <div class="mb-4">
+      <router-link v-for="item in versions" :key="item" class="btn btn-dark mr-1 mb-2" :to="'/' + item + '/' + $route.params.section">{{ item.charAt(0).toUpperCase() + item.slice(1) }}</router-link>
+    </div>
+    <div class="mb-4">
+      <router-link v-for="item in sections" :key="item" class="btn btn-dark mr-1 mb-2" :to="'/' + $route.params.version + '/' + item">{{ item.charAt(0).toUpperCase() + item.slice(1) }}</router-link>
+    </div>
     <h1>{{ $route.params.version }} {{ $route.params.section }}</h1>
   </div>`
 }
