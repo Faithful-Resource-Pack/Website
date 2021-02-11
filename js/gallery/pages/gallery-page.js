4a666e356cac0412920265a1482e9e6b70c08167
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-02-11 14:24:27 +0100

    Add gallery item styles

--- js/gallery/pages/gallery-page.js
+++ js/gallery/pages/gallery-page.js
@@ -5,6 +5,7 @@ export default {
   },
   data() {
     return {
+      dummyArray: ['Title1', 'Title2', 'Title3', 'Title4', 'Title5', 'Title6', 'Title7', 'Title8', 'Title9'],
       versions: ['java-32x', 'java-64x', 'bedrock-32x', 'bedrock-64x', 'dungeons', 'education'],
       sections: ['armor', 'blocks', 'colormaps', 'entities', 'environment', 'font', 'gui', 'items', 'map', 'mob-effects', 'paintings', 'particles', 'others']
     }
@@ -17,6 +18,16 @@ export default {
     <div class="mb-4">
       <router-link v-for="item in sections" :key="item" class="btn btn-dark mr-1 mb-2" :to="'/' + $route.params.version + '/' + item">{{ item.charAt(0).toUpperCase() + item.slice(1) }}</router-link>
     </div>
-    <h1>{{ $route.params.version }} {{ $route.params.section }}</h1>
+    <div class="res-grid-6">
+      <div v-for="item in dummyArray" :key="item" class="gallery-item">
+        <img src="https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/master/assets/minecraft/textures/block/barrel_top.png" loading="lazy" :alt="item">
+        <i class="fas fa-download"></i>
+        <i class="fas fa-expand"></i>
+        <div class="info">
+          <p>{{ item }}</p>
+          <p>Location</p>
+        </div>
+      </div>
+    </div>
   </div>`
 }
