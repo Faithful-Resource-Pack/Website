36f43150bcc940ec68eb2a1540b751fda289fe1a
Dominik <36272047+Domi04151309@users.noreply.github.com>
2021-02-13 15:17:31 +0100

    Gallery revamp2 (#11)
    * Add basic navigation
    
    * More elegant navigation
    
    * Add complete navigation
    
    * Add gallery item styles
    
    * Make a mess
    
    * Make more buttons work
    
    * Polish cow
    
    * Add artist credits
    
    * Clean up
    
    * Update gallery.css
    
    * Fix modal
    
    * Fix texture detection
    
    * remove colormap folder because we won't ever do it
    
    * Add search bar
    
    * Show more info in fullscreen mode
    
    * Searching for artists and better caching
    
    * Improve behavior on mobile
    
    * JavaScript is a mess
    
    Co-authored-by: Robert Reichel <robertreich1212@gmail.com>

---
+++ js/gallery/index.js
@@ -0,0 +1,15 @@
+const GalleryPage = () => import('./pages/gallery-page.js')
+
+Vue.config.devtools = location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+
+const routes = [
+  { path: '*', redirect: '/java-32x/block' },
+  { path: '/:version/:section', component: GalleryPage }
+]
+
+const router = new VueRouter({ routes })
+
+new Vue({
+  router,
+  el: '#app'
+})
