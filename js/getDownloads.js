540ab65c83ca76a0eae4d127cea51dc2f761ff87
Robert Reichel <robertreich1212@gmail.com>
2021-02-13 20:50:52 +0100

    get ready for beta 2

--- js/getDownloads.js
+++ js/getDownloads.js
@@ -23,16 +23,18 @@ async function getDownloads(id, release, org, repo, assetIndex = 0) {
 }
 
 async function run() {
-	await getDownloads('c32-j-beta-1', 1, DEFAULT_ORG, 'Compliance-Java-32x', 1)
-	await getDownloads('c32-j-alpha-7', 2, DEFAULT_ORG, 'Compliance-Java-32x')
-	await getDownloads('c32-j-alpha-6', 3, DEFAULT_ORG, 'Compliance-Java-32x')
-	await getDownloads('c32-j-alpha-5', 4, DEFAULT_ORG, 'Compliance-Java-32x')
-	await getDownloads('c32-j-alpha-4', 5, DEFAULT_ORG, 'Compliance-Java-32x')
-	await getDownloads('c32-j-alpha-3', 6, DEFAULT_ORG, 'Compliance-Java-32x')
-	await getDownloads('c32-j-alpha-2', 7, DEFAULT_ORG, 'Compliance-Java-32x')
-	await getDownloads('c32-j-alpha-1', 8, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-beta-2', 1, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-beta-1', 2, DEFAULT_ORG, 'Compliance-Java-32x', 1)
+	await getDownloads('c32-j-alpha-7', 3, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-6', 4, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-5', 5, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-4', 6, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-3', 7, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-2', 8, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-1', 9, DEFAULT_ORG, 'Compliance-Java-32x')
 
-	await getDownloads('c32-b-beta-1', 1, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-b-beta-2', 1, DEFAULT_ORG, 'Compliance-Bedrock-32x')
+	await getDownloads('c32-b-beta-1', 2, DEFAULT_ORG, 'Compliance-Java-32x')
 	await getDownloads('c64-b-alpha-2', 1, DEFAULT_ORG, 'Compliance-Bedrock-64x')
 
 	await getDownloads('c32-d-beta-1', 1, DUNGEONS_ORG, 'Resource-Pack')
