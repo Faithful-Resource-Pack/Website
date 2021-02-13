f6ba997088cdbb0e1837d77289cd8ea50ba24a20
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-02-13 17:27:36 +0100

    Add more counters

--- js/getDownloads.js
+++ js/getDownloads.js
@@ -1,5 +1,6 @@
 // Used in download.html
 const DEFAULT_ORG = 'Compliance-Resource-Pack'
+const DUNGEONS_ORG = 'Compliance-Dungeons'
 
 let cache = {}
 
@@ -15,8 +16,25 @@ async function getJson(url) {
 	}
 }
 
-async function getDownloads(id, release, org, repo) {
+async function getDownloads(id, release, org, repo, assetIndex = 0) {
 	let	url = `https://api.github.com/repos/${org}/${repo}/releases`
 	let data = await getJson(url)
-	document.getElementById(id).innerHTML = data[release - 1]['assets'][0]['download_count'] + '<i style="margin-left: 10px" class="fas fa-download"></i>'
+	document.getElementById(id).innerHTML = data[release - 1]['assets'][assetIndex]['download_count'] + '<i style="margin-left: 10px" class="fas fa-download"></i>'
 }
+
+async function run() {
+	await getDownloads('c32-j-beta-1', 1, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-7', 2, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-6', 3, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-5', 4, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-4', 5, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-3', 6, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-2', 7, DEFAULT_ORG, 'Compliance-Java-32x')
+	await getDownloads('c32-j-alpha-1', 8, DEFAULT_ORG, 'Compliance-Java-32x')
+
+	await getDownloads('c32-b-beta-1', 1, DEFAULT_ORG, 'Compliance-Java-32x', 1)
+
+	await getDownloads('c32-d-beta-1', 1, DUNGEONS_ORG, 'Resource-Pack')
+}
+
+run()
