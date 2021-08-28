f1578159c4160f715de35ad055a9f017b78b0eba
Julien <49886317+Juknum@users.noreply.github.com>
2021-08-28 19:28:41 +0200

    optimize code & sort addons by name

--- js/addons/addon-page.js
+++ js/addons/addon-page.js
@@ -1,124 +1,90 @@
 /* global fetch */
 
+const AddonGrid = () => import('./addon-grid.js')
+
+function sortObj(obj) {
+  return Object.keys(obj).sort().reduce((res, key) => {
+    res[key] = obj[key]
+    return res
+  }, {});
+}
+
 export default {
   name: 'addon-page',
+  components: {
+    AddonGrid
+  },
   template: `
-    <v-container
-      style="max-width: 1140px; padding-top: 100px; padding-bottom: 100px"
-    >
-      <h2 class="text-center" style="font-size: 4.8rem; font-weight: 300; line-height: 1.2; margin-bottom: 3rem; margin-top: 3rem">Add-ons</h2>
+  <v-container
+    style="max-width: 1140px; padding-top: 100px; padding-bottom: 100px"
+  >
+    <h2 class="text-center" style="font-size: 4.8rem; font-weight: 300; line-height: 1.2; margin-bottom: 3rem; margin-top: 3rem">Add-ons</h2>
 
-      <h3 class="text-center" v-if="Object.keys(fav).length">Favorites</h3>
-      <div class="card card-body" v-if="Object.keys(fav).length">
-        <div class="res-grid-3">
-          <div v-for="(addon, index) in fav" class="hovering-effect" style="margin-bottom: calc(-28px)" v-if="addon.status === 'approved'">
-            <router-link class="card img-card" :to="addon.id">
-              <img :src="addon.images.header">
-              <div class="img-card-shadow"></div>
-              <h3>{{ addon.title }}</h3>
-              <div class="addon-flags" style="margin-bottom: 5px;">
-                <img style="margin-bottom: 5px;" v-if="addon.type.includes('Java')" :src="java" alt="available for Java Edition" loading="lazy">
-                <img style="margin-bottom: 5px;" v-if="addon.type.includes('Bedrock')" :src="bedrock" alt="available for Bedrock Edition" loading="lazy">
-                <img style="margin-bottom: 5px;" v-if="addon.optifine" :src="optifine" alt="requires optifine" loading="lazy">
-              </div>
-              <div class="addon-tags">
-                <p style="margin-bottom: 5px; margin-right: 5px;" v-if="addon.type.includes('32x')" >32x</p>
-                <p style="margin-bottom: 5px;" v-if="addon.type.includes('64x')" >64x</p>
-              </div>
-            </router-link>
-            <v-btn
-              @click="checkFav(addon)"
-              small
-              color="#ff3333"
-              icon
-
-              style="position: relative; top: calc(-100% + 2px + 28px); left: 2px;"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-          </div>
-        </div>
-      </div>
-
-      <template v-if="Object.keys(fav).length">
-        <br>
-        <h3 class="text-center">All</h3>
-      </template>
-
-      <div class="card card-body">
-        <h4 class="text-center">Search</h4>
-        <div class="checkbox-container">
-          <v-checkbox
-            v-for="type in editions"
-            v-model="selectedEditions"
-            :label="type"
-            :value="type"
-            color="primary"
-            @change="startSearch"
-          >
-          </v-checkbox>
-          <v-checkbox
-            v-for="type in res"
-            v-model="selectedRes"
-            :label="type"
-            :value="type"
-            color="primary"
-            @change="startSearch"
-          >
-          </v-checkbox>
-        </div>
-        <v-text-field
-          v-model="search"
-          :append-outer-icon="search ? 'mdi-send' : undefined"
-          filled
-          clear-icon="mdi-close"
-          clearable
-          color="primary"
-          placeholder="Search add-on name"
-          type="text"
-          v-on:keyup.enter="startSearch"
-          @click:append-outer="startSearch"
-          @click:clear="clearSearch"
-        ></v-text-field>
-      </div>
+    <template v-if="Object.keys(fav).length">
+      <h3 class="text-center">Favorites</h3>
+      <addon-grid
+        :key="Object.keys(fav).length"
+        :addons="fav"
+        :action="checkFav"
+        icon="mdi-close"
+        iconColor="#ff3333"
+        :addonsFav="fav"
+      >
+      </addon-grid>
       <br>
-      <div class="card card-body">
-        <div class="res-grid-3" v-if="Object.keys(searchedAddons).length">
-          <div v-for="(addon, index) in searchedAddons" class="hovering-effect" style="margin-bottom: calc(-28px)" v-if="addon.status === 'approved'">
-            <router-link class="card img-card" :to="addon.id">
-              <img :src="addon.images.header">
-              <div class="img-card-shadow"></div>
-              <h3>{{ addon.title }}</h3>
-              <div class="addon-flags" style="margin-bottom: -5px;">
-                <img style="margin-bottom: 5px;" v-if="addon.type.includes('Java')" :src="java" alt="available for Java Edition" loading="lazy">
-                <img style="margin-bottom: 5px;" v-if="addon.type.includes('Bedrock')" :src="bedrock" alt="available for Bedrock Edition" loading="lazy">
-                <img style="margin-bottom: 5px;" v-if="addon.optifine" :src="optifine" alt="requires optifine" loading="lazy">
-              </div>
-              <div class="addon-tags">
-                <p style="margin-bottom: 5px; margin-right: 5px" v-if="addon.type.includes('32x')" >32x</p>
-                <p style="margin-bottom: 5px;" v-if="addon.type.includes('64x')" >64x</p>
-              </div>
-            </router-link>
-            <v-btn
-              @click="checkFav(addon)"
-              small
-              :color="fav[addon.id] ? '#FFC83D' : 'rgba(0, 0, 0, 0.5)' "
-              icon
-
-              style="relative; top: calc(-100% + 2px + 28px); left: 2px;"
-            >
-              <v-icon>mdi-star</v-icon>
-            </v-btn>
-          </div>
-        </div>
-        <div v-else-if="loading">
-          <p align="center"><i>Loading Add-ons, please wait...</i></p>
-        </div>
-        <div v-else>
-          <p align="center"><i>No results found {{ search ? 'for ' + search : '' }}</i></p>
-        </div>
+      <h3 class="text-center">All</h3>
+    </template>
+    
+    <div class="card card-body">
+      <h4 class="text-center">Search</h4>
+      <div class="checkbox-container">
+        <v-checkbox
+          v-for="type in editions"
+          v-model="selectedEditions"
+          :label="type"
+          :value="type"
+          color="primary"
+          @change="startSearch"
+        >
+        </v-checkbox>
+        <v-checkbox
+          v-for="type in res"
+          v-model="selectedRes"
+          :label="type"
+          :value="type"
+          color="primary"
+          @change="startSearch"
+        >
+        </v-checkbox>
       </div>
-    </v-container>`,
+      <v-text-field
+        v-model="search"
+        :append-outer-icon="search ? 'mdi-send' : undefined"
+        filled
+        clear-icon="mdi-close"
+        clearable
+        color="primary"
+        placeholder="Search add-on name"
+        type="text"
+        v-on:keyup.enter="startSearch"
+        @click:append-outer="startSearch"
+        @click:clear="clearSearch"
+      ></v-text-field>
+    </div>
+    <br>
+
+    <addon-grid
+      :key="Object.keys(fav).length"
+      :addons="searchedAddons"
+      :action="checkFav"
+      icon="mdi-star"
+      iconColor="#ffc83d"
+      :addonsFav="fav"
+    >
+    </addon-grid>
+
+  </v-container>
+  `,
   data () {
     return {
       addons: {},
@@ -186,11 +152,12 @@ export default {
     fetch('https://database.compliancepack.net/firestorm/files/addons.json')
       .then(res => res.json())
       .then(data => {
-        this.addons = data
-        this.searchedAddons = data
+        this.addons = sortObj(data)
         this.loading = false
-
+        
         for (const addonID in this.addons) this.addons[addonID].id = addonID // fix missing ID (property value)
+  
+        this.searchedAddons = this.addons
       })
 
     this.fav = JSON.parse(window.localStorage.getItem('favAddons') || '{}')
