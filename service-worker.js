45909b9260358ee44720fc718af2fcacd58e0bed
Robert Reichel <robertreich1212@gmail.com>
2021-12-26 21:29:07 +0100

    PWA attempt 1

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+console.log('Hello from service-worker.js');
+
+/*this.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/index.html'
+        ]);
+      })
+    );
+  });*/
