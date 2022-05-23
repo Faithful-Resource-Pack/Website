0de7a118a9fdf8ab4b740a1e46ad99860045d91c
Julien Constant <julienconstant190@gmail.com>
2022-05-23 15:53:27 +0200

    responsive navbar

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -2,23 +2,67 @@
   <v-app-bar
     dense
     shaped
+    v-if="$vuetify.display.mdAndUp"
   >
     <v-row
       no-gutters
       class="justify-center"
     >
-      <v-btn-localized to="/">{{ $t('navbar.btn_home') }}</v-btn-localized>
-      <dropdown-menu :menus="getMenus('downloads')" :name="$t('navbar.btn_downloads')"></dropdown-menu>
-      <dropdown-menu :menus="getMenus('addons')" :name="$t('navbar.btn_addons')"></dropdown-menu>
+      <v-col
+        cols="5"
+        class="d-flex justify-end"
+      >
+        <v-btn-localized to="/">{{ $t('navbar.btn_home') }}</v-btn-localized>
+        <dropdown-menu :menus="getMenus('downloads')" :name="$t('navbar.btn_downloads')"></dropdown-menu>
+        <dropdown-menu :menus="getMenus('addons')" :name="$t('navbar.btn_addons')"></dropdown-menu>
+      </v-col>
 
-      <img class="mx-2" aspect-ratio="1" height="32" :src="FAITHFUL_LOGO" alt="" />
+      <v-col class="d-flex justify-center" style="max-width: 36px;">
+        <localized-link to="/" class="d-flex align-center">
+          <img aspect-ratio="1" height="32" :src="FAITHFUL_LOGO" alt="" />
+        </localized-link>
+      </v-col>
 
-      <dropdown-menu :menus="getMenus('tweaks')" :name="$t('navbar.btn_tweaks')"></dropdown-menu>
-      <dropdown-menu :menus="getMenus('modding')" :name="$t('navbar.btn_modding')"></dropdown-menu>
-      <v-btn v-if="!user.isUserLoggedIn()" @click="user.login()">{{ $t('navbar.btn_login') }}</v-btn>
-      <v-btn-localized v-else to="/profile">{{ $t('navbar.btn_profile') }}</v-btn-localized>
+      <v-col
+        cols="5"
+        class="d-flex justify-start"
+      >
+        <dropdown-menu :menus="getMenus('tweaks')" :name="$t('navbar.btn_tweaks')"></dropdown-menu>
+        <dropdown-menu :menus="getMenus('modding')" :name="$t('navbar.btn_modding')"></dropdown-menu>
+        <v-btn v-if="!user.isUserLoggedIn()" @click="user.login()">{{ $t('navbar.btn_login') }}</v-btn>
+        <v-btn-localized v-else to="/profile">{{ $t('navbar.btn_profile') }}</v-btn-localized>
+      </v-col>
     </v-row>
   </v-app-bar>
+
+  <v-app-bar
+    v-else
+    :height="showNavbarMenu ? 400 : 64"
+  >
+    <v-col style="padding-right: 0;">
+      <v-row style="width: 100%; justify-content: space-between; margin-left: 0;">
+        <localized-link to="/" class="d-flex align-center">
+          <img :src="FAITHFUL_TEXT" alt="" height="24">
+        </localized-link>
+
+        <v-btn icon="mdi-menu" @click="showNavbarMenu = !showNavbarMenu"></v-btn>
+      </v-row>
+      <v-row v-if="showNavbarMenu" class="mt-5">
+        <v-list>
+          <v-list-item><v-btn-localized to="/">{{ $t('navbar.btn_home') }}</v-btn-localized></v-list-item>
+          <v-list-item><dropdown-menu :menus="getMenus('downloads')" :name="$t('navbar.btn_downloads')"></dropdown-menu></v-list-item>
+          <v-list-item><dropdown-menu :menus="getMenus('addons')" :name="$t('navbar.btn_addons')"></dropdown-menu></v-list-item>
+          <v-list-item><dropdown-menu :menus="getMenus('tweaks')" :name="$t('navbar.btn_tweaks')"></dropdown-menu></v-list-item>
+          <v-list-item><dropdown-menu :menus="getMenus('modding')" :name="$t('navbar.btn_modding')"></dropdown-menu></v-list-item>
+          <v-list-item>
+            <v-btn v-if="!user.isUserLoggedIn()" @click="user.login()">{{ $t('navbar.btn_login') }}</v-btn>
+            <v-btn-localized v-else to="/profile">{{ $t('navbar.btn_profile') }}</v-btn-localized>
+          </v-list-item>
+        </v-list>
+      </v-row>
+    </v-col>
+  </v-app-bar>
+
   <!-- fake margin for the fixed navbar -->
   <div style="height: 80px;"></div>
 </template>
@@ -37,6 +81,8 @@ export default defineComponent({
   },
   data() {
     return {
+      showNavbarMenu: false,
+      FAITHFUL_TEXT: 'https://database.faithfulpack.net/images/branding/wordmarks/default/flat/faithful_flat.png',
       FAITHFUL_LOGO: 'https://database.faithfulpack.net/images/branding/logos/transparent/512/plain_logo.png',
       FAITHFUL_32X_LOGO: 'https://database.faithfulpack.net/images/branding/logos/transparent/512/f32_logo.png',
       FAITHFUL_64X_LOGO: 'https://database.faithfulpack.net/images/branding/logos/transparent/512/f64_logo.png',
