d786c35aa6af34e3e1ed980d0c9a3ee2b5c71ad0
Robert <contact.e3nk6@aleeas.com>
2022-12-17 19:58:24 +0100

    better add-ons background

--- src/routes/add-ons/+page.svelte
+++ src/routes/add-ons/+page.svelte
@@ -3,46 +3,45 @@
 	import AddonResults from "$components/addons/AddonResults.svelte";
 	import AddonSearch from "$components/addons/AddonSearch.svelte";
 
-    const text_title =  'Add-ons';
+    const text_title = 'Add-ons';
     const text_desc = `Customise your game experience!\nBrought to you by the community.`.replace('\n', '<br>');
     const text_how_to_install = 'How to install add-ons';
 </script>
 
-<div id="container">
-    <div id="addon_header" class="text-center">
-        <h1 class="title">{ text_title }</h1>
-        <p>{ @html text_desc }</p>
-        <p><i>{ text_how_to_install }</i></p>
-    </div>
+<img class="addon_background" src="/images/background/Add-ons.png" alt="Add-ons background">
+<div id="addon_header" class="text-center">
+    <h1 class="title">{ text_title }</h1>
+    <p>{ @html text_desc }</p>
+    <p><i>{ text_how_to_install }</i></p>
+    <h3>placeholder text because the background would go over the footer lol</h3>
+    <h3>placeholder text because the background would go over the footer lol</h3>
+</div>
     
-    <div id="addon_container" class="container">
-        <AddonCategories />
-        <div id="addon_main">
-            <div class="addon_search">
-                <AddonSearch />
-            </div><div class="addon_main">
-                <AddonResults class="addon_main" />
-            </div>
+<div id="addon_container" class="container">
+    <AddonCategories />
+    <div id="addon_main">
+        <div class="addon_search">
+            <AddonSearch />
+        </div><div class="addon_main">
+            <AddonResults class="addon_main" />
         </div>
     </div>
 </div>
 
 <style lang="scss">
-    #container {
-        background-color: white;
-        padding-bottom: 50px;
-    }
-
     #addon_header {
-        padding: 1px 50px;
-        margin-bottom: 50px;
-        background: url(https://static.wixstatic.com/media/89b89d_e287df2f94854c9fb28d4e62254e96c7~mv2.png/v1/fill/w_1920,h_279,al_b,q_90,usm_0.66_1.00_0.01,enc_auto/89b89d_e287df2f94854c9fb28d4e62254e96c7~mv2.png);
-        background-position: bottom center;
-        background-size: cover;
+        position: relative;
 
         p i {
             text-decoration: underline;
         }
+    }
+
+    .addon_background {
+        width: 100%;
+        position: absolute;
+        mask: linear-gradient(180deg, #000, transparent);
+        filter: saturate(0.7);
     }
     
     #addon_container > *{
