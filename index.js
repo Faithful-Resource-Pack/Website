50b04719b0e4ef3435f4093a88ccd84baa253152
Yann Le Vagueres <yann.lv@live.fr>
2023-01-21 19:47:25 +0100

    generate post json

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { default: axios } = require('axios')
 const express = require('express')
-const fs = require('fs')
+const fs = require('fs').promises
+const { readFileSync } = require('fs')
+const createDOMPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const { parse } = require('path')
 
 require('dotenv').config()
+
+const window = new JSDOM('').window
+const DOMPurify = createDOMPurify(window)
 
 const { ID_FIELD } = require('./js/firestorm')
 const firestorm = require('./js/firestorm')
@@ -22,7 +29,7 @@ firestorm.address(process.env.FIRESTORM_URL)
 const users = firestorm.collection("users")
 
 app.get(['/coffee', '/teapot'], (req, res) => {
-    let data = fs.readFileSync(COFFEE_PAGE, 'utf8')
+    let data = readFileSync(COFFEE_PAGE, 'utf8')
 
     let title_el = data.match(/<title>(.+)<\/title>/)
     if(title_el) {
@@ -34,6 +41,134 @@ app.get(['/coffee', '/teapot'], (req, res) => {
 
     res.status(418).send(data)
     res.end()
+})
+
+/**
+ * @typedef {[string, boolean, number?]} Extra
+ */
+
+/**
+ * @type {Extra[]}
+ */
+const EXTRACT = [
+  ['title', true],
+  ['permalink', true],
+  ['comments', true],
+  ['comments-id', true],
+  ['header-img', true],
+  ['long_text', false],
+  ['single-changelog', true],
+  ['expanded-changelog', true],
+  ['downloads', false],
+  ['download', false],
+  ['main_changelog', true],
+]
+
+/**
+ * definitions
+ * @param {string[]} input lines
+ * @param {Extra} extra extra def
+ * 
+ * @return {{[tag]: string} | undefined} 
+ */
+function extract(input, [tag, oneline, accepted_empty_lines]) {
+  let i = input.findIndex(l => l.startsWith(`${tag}:`))
+  if(i === -1) return undefined
+
+  if(oneline) {
+    let line = input[i]
+    let line_cleaned = line.replace(`${tag}:`, '').trim()
+    if(line_cleaned.startsWith('"')) line_cleaned = line_cleaned.substring(1)
+    if(line_cleaned.endsWith('"')) line_cleaned = line_cleaned.slice(0, -1)
+    return line_cleaned
+  }
+
+  accepted_empty_lines = accepted_empty_lines || 1;
+
+  let res = input[i].replace(`${tag}:`, '');
+  i++;
+
+  let running = true;
+  let empty_lines = 0;
+  while(running && i < input.length) {
+    line = input[i];
+
+    let is_empty = line.trim() === '';
+    if(!is_empty) empty_lines = 0;
+    else empty_lines++;
+
+    if(empty_lines >= accepted_empty_lines) {
+      running = false;
+    } else {
+      res += '\n' + line;
+    }
+
+    i++;
+  }
+
+  let line_cleaned = res.trim()
+  if(line_cleaned.startsWith('"')) line_cleaned = line_cleaned.substring(1)
+  if(line_cleaned.endsWith('"')) line_cleaned = line_cleaned.slice(0, -1)
+
+  return line_cleaned;
+}
+
+const EXTRACTED_POSTS_PATH = `${__dirname}/posts.json`
+let posts_map = undefined
+let posts_map_loading = false
+function generate_posts_json() {
+  if(posts_map_loading) return Promise.reject(new Error('Loading'))
+  if(!!posts_map) return Promise.resolve(posts_map)
+
+  posts_map_loading = true
+
+  const dir_posts = `${__dirname}/_posts`
+  return fs.readdir(dir_posts,  {withFileTypes: true})
+  .then(dirs => dirs.filter(e => e.isFile() && e.name.endsWith('.md') && !e.name.startsWith("_")))
+  .then(md_files => Promise.all(
+    md_files.map(
+      e => Promise.all([e, fs.readFile(`${dir_posts}/${e.name}`, 'utf8')])
+    )
+  ))
+  .then(md_contents => {
+    return md_contents.map(([e, md]) => {
+      let tmp = {
+        name: parse(e.name).name
+      }
+
+      let lines = md.split('\n').filter(l => l !== '---')
+      EXTRACT.forEach((e) => {
+        extracted = extract(lines, e)
+        if(['long_text', 'downloads', 'main_changelog'].indexOf(e[0]) !== -1 && !!extracted)
+          tmp[e[0]] = DOMPurify.sanitize(extracted)
+        else
+          tmp[e[0]] = extracted
+      })
+
+      return tmp
+    }).reduce((acc, cur) => {
+      acc[cur.name] = cur;
+      return acc;
+    }, {});
+  })
+  .then(result => {
+    posts_map = result
+    posts_map_loading = false
+
+    fs.writeFile(EXTRACTED_POSTS_PATH, JSON.stringify(result))
+    
+    return result
+  })
+}
+
+app.get('/posts.json', (_, res) => {
+  generate_posts_json()
+  .then(() => {
+    res.sendFile(EXTRACTED_POSTS_PATH)
+  })
+  .catch(err => {
+    res.status(403).send(err.message).end()
+  })
 })
 
 app.use(express.static(__dirname + '/_site/', {
@@ -99,7 +234,7 @@ app.get('/addons/:name/?', (req, res, next) => {
     dataReplaced.url = `${req.originalUrl}`
 
     // load addon post page
-    let data = fs.readFileSync(ADDON_PAGE, 'utf8')
+    let data = await fs.readFile(ADDON_PAGE, 'utf8')
 
     // replace header if existing
     if(header_url) {
@@ -121,7 +256,7 @@ app.get('/addons/:name/?', (req, res, next) => {
     res.send(data)
     res.end()
   }).catch(err => {
-    console.trace(err)
+    console.error(err)
     next()
   })
 })
