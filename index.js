b9978290c9d4bb0057a128f561af851c9767a8aa
Evorp <3vorpgaming@gmail.com>
2024-10-02 08:22:52 -0700

    replace jekyll post system with vue/express

--- index.js
+++ index.js
@@ -20,10 +20,14 @@ app.disable("x-powered-by");
 
 const NOT_FOUND_PAGE = __dirname + "/_site/404.html";
 
+const POST_PAGE = __dirname + "/_site/post.html";
 const ADDON_PAGE = __dirname + "/_site/addon.html";
 const COFFEE_PAGE = __dirname + "/_site/coffee.html";
-const ADDON_REPLACE_TOKEN = (token) => `%${token}%`;
-const ADDON_FIELD_REPLACE = ["url", "name", "description", "authors", "header_img"];
+const replaceTemplateToken = (token) => `%${token}%`;
+const cleanPostData = (post) => JSON.stringify(post).replaceAll("\\", "\\\\");
+
+const ADDON_REPLACE_FIELDS = ["url", "name", "description", "authors", "header_img"];
+const POST_REPLACE_FIELDS = ["permalink", "title", "description", "header_img"];
 
 app.get(["/coffee", "/teapot"], async (req, res) => {
 	let data = await readFile(COFFEE_PAGE, "utf8");
@@ -40,9 +44,9 @@ app.get(["/coffee", "/teapot"], async (req, res) => {
 	res.end();
 });
 
+const POST_IMPORT_PATH = `${__dirname}/posts`;
 const EXTRACTED_POSTS_PATH = `${__dirname}/posts.json`;
 let postsMap = undefined;
-let postsMapLoading = false;
 
 function walkSync(dir, filelist = []) {
 	// add trailing slash if not present
@@ -59,50 +63,46 @@ function walkSync(dir, filelist = []) {
 function generatePostJSON() {
 	// use raw promise for better error handling in reduce callback
 	return new Promise((resolve, reject) => {
-		if (postsMapLoading) reject(new Error("Loading"));
 		if (!!postsMap) resolve(postsMap);
 
-		postsMapLoading = true;
-
-		const dirPosts = `${__dirname}/_posts`;
-		const paths = walkSync(dirPosts).filter(
+		const paths = walkSync(POST_IMPORT_PATH).filter(
 			(d) => d.endsWith(".md") && !d.endsWith("README.md"),
 		);
-		const result = paths
-			.sort((a, b) => {
-				const aName = a.split("/").at(-1);
-				const bName = b.split("/").at(-1);
-				return aName.localeCompare(bName);
-			})
-			.reduce((acc, cur) => {
-				const filename = cur.split("/").at(-1);
-				let obj;
-				try {
-					obj = matter.read(cur).data;
-				} catch (err) {
-					reject(err);
-				}
-				// unneeded implementation detail
-				delete obj.layout;
-				const [y, m, d, ...name] = filename.split("-");
-				obj.date = [y, m, d].join("-");
-				const key = name.join("-").replace(".md", "");
-				if (acc[key]) reject(new Error(`Duplicate post key ${key}`));
-				acc[key] = obj;
-				return acc;
-			}, {});
+		const result = paths.reduce((acc, cur) => {
+			const filename = cur.split("/").at(-1);
+			let obj;
+			try {
+				obj = matter.read(cur).data;
+			} catch (err) {
+				reject(err);
+			}
+
+			// clean up jekyll holdovers
+			delete obj.layout;
+			const headerImg = obj["header-img"];
+			obj.header_img = headerImg;
+			delete obj["header-img"];
+
+			// take date from filename
+			const [y, m, d] = filename.split("-");
+			obj.date = [y, m, d].join("-");
+			const key = obj.permalink;
+			if (acc[key]) reject(new Error(`Duplicate post key ${key}`));
+			acc[key] = obj;
+			return acc;
+		}, {});
 
 		postsMap = result;
-		postsMapLoading = false;
 
 		writeFile(EXTRACTED_POSTS_PATH, JSON.stringify(result)).then(() => resolve(result));
 	});
 }
 
-app.get("/posts.json", cors(corsOptions), (_, res) => {
-	generatePostJSON()
-		.then(() => res.sendFile(EXTRACTED_POSTS_PATH))
-		.catch((err) => res.status(403).send(err.message).end());
+app.get("/posts.json", cors(corsOptions), async (_, res) => {
+	const file = await readFile(EXTRACTED_POSTS_PATH, { encoding: "utf8" }).catch(() => null);
+	if (file) return res.send(file).end();
+	const json = await generatePostJSON();
+	res.send(JSON.stringify(json)).end();
 });
 
 app.use(
@@ -110,6 +110,29 @@ app.use(
 		extensions: ["html", "htm"],
 	}),
 );
+
+// posts (match all routes and continue if no matching post is found)
+app.get("*", async (req, res, next) => {
+	const file = await readFile(EXTRACTED_POSTS_PATH, { encoding: "utf8" }).catch(() => null);
+	// posts not loaded yet
+	if (!file) return next();
+
+	const json = JSON.parse(file);
+	// remove last character (permalinks don't end with a slash)
+	if (req.url.endsWith("/")) req.url = req.url.slice(0, -1);
+	const post = json[req.url];
+	// post doesn't exist under path, probably another page
+	if (!post) return next();
+
+	let data = await readFile(POST_PAGE, { encoding: "utf8" });
+	POST_REPLACE_FIELDS.forEach((token) => {
+		data = data.replaceAll(replaceTemplateToken(token), post[token]);
+	});
+	// send stringified contents
+	data = data.replaceAll("%postData%", cleanPostData(post));
+	res.send(data);
+	res.end();
+});
 
 // redirect addon page because it is a "template" page
 app.get("/addon", (req, res, next) => {
@@ -165,7 +188,7 @@ app.get("/addons/:name/?", async (req, res) => {
 
 	const authorArray = authors.map((user) => user.username).filter((e) => e);
 
-	const dataReplaced = ADDON_FIELD_REPLACE.reduce((acc, token) => {
+	const dataReplaced = ADDON_REPLACE_FIELDS.reduce((acc, token) => {
 		acc[token] = addon[token];
 		return acc;
 	}, {});
@@ -174,23 +197,23 @@ app.get("/addons/:name/?", async (req, res) => {
 	dataReplaced.url = `${req.originalUrl}`;
 
 	// load addon post page
-	let data = await readFile(ADDON_PAGE, "utf8");
+	let data = await readFile(ADDON_PAGE, { encoding: "utf8" });
 
 	// replace header if existing
 	if (headerURL) dataReplaced.header_img = headerURL;
 
 	// replace all header values
-	ADDON_FIELD_REPLACE.forEach((token) => {
-		data = data.replaceAll(ADDON_REPLACE_TOKEN(token), dataReplaced[token]);
+	ADDON_REPLACE_FIELDS.forEach((token) => {
+		data = data.replaceAll(replaceTemplateToken(token), dataReplaced[token]);
 	});
 
 	// replace script value
 	//! please use Node v15+ for support of replaceAll
 	data = data
-		.replaceAll("'" + ADDON_REPLACE_TOKEN("data.addon") + "'", JSON.stringify(addon))
-		.replaceAll("'" + ADDON_REPLACE_TOKEN("data.authors") + "'", JSON.stringify(authors))
-		.replaceAll("'" + ADDON_REPLACE_TOKEN("data.slug") + "'", JSON.stringify(req.params.name))
-		.replaceAll("'" + ADDON_REPLACE_TOKEN("data.files") + "'", JSON.stringify(addon.files));
+		.replaceAll("'" + replaceTemplateToken("data.addon") + "'", JSON.stringify(addon))
+		.replaceAll("'" + replaceTemplateToken("data.authors") + "'", JSON.stringify(authors))
+		.replaceAll("'" + replaceTemplateToken("data.slug") + "'", JSON.stringify(req.params.name))
+		.replaceAll("'" + replaceTemplateToken("data.files") + "'", JSON.stringify(addon.files));
 
 	res.send(data);
 	res.end();
@@ -198,6 +221,9 @@ app.get("/addons/:name/?", async (req, res) => {
 
 app.use((_req, res) => res.status(404).sendFile(NOT_FOUND_PAGE));
 
-app.listen(process.env.PORT, () =>
-	console.log(`Website listening at http://localhost:${process.env.PORT} in ${__dirname}`),
-);
+app.listen(process.env.PORT, () => {
+	console.log(`Website listening at http://localhost:${process.env.PORT} in ${__dirname}`);
+	generatePostJSON()
+		.then(() => console.log("Post JSON initialized!"))
+		.catch((err) => console.error("Failed to load post JSON:\n", err));
+});
