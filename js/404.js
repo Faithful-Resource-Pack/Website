a54cb35212e8d79194960e3d942a63bf5328c130
Evorp <3vorpgaming@gmail.com>
2024-10-03 16:52:19 -0700

    use some dynamic urls on 404 page
    the animated stuff can't use api urls but this is better

--- js/404.js
+++ js/404.js
@@ -1,27 +1,25 @@
 const FRAME_TIME = 12;
-const tnt = document.getElementById('tnt');
-const diamond = document.getElementById('diamond');
-const explosion = document.getElementById('explosion');
+const tnt = document.getElementById("tnt");
+const diamond = document.getElementById("diamond");
+const explosion = document.getElementById("explosion");
 let exploded = false;
 
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-document.body.classList.add('error')
+document.body.classList.add("error");
 
 async function boom() {
 	if (!exploded) {
 		for (let i = 0; i < 8; i++) {
-			await sleep(FRAME_TIME*25);
+			await sleep(FRAME_TIME * 25);
 			if (i % 2 === 0) tnt.src = "../image/404/tnt_side_on.png";
 			else tnt.src = "../image/404/tnt_side.png";
 		}
 		explosion.classList.remove("hidden");
 		tnt.classList.add("hidden");
 		for (let i = 0; i <= 15; i++) {
-			await sleep(FRAME_TIME*1.3);
-			explosion.src = "../image/404/explosion_" + i + ".png";
+			await sleep(FRAME_TIME * 1.3);
+			explosion.src = `../image/404/explosion_${i}.png`;
 		}
 		diamond.classList.remove("hidden");
 		explosion.classList.add("hidden");
