ef8055e893d96424b7d7c665a6eb41740f662ffe
Evorp <3vorpgaming@gmail.com>
2024-10-27 12:44:25 -0700

    how does this break everything

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,7 @@
 /* global localStorage */
 
 const css = document.getElementById('theme')
-const btn = document.getElementById('theme-btn')
+const btn = document.getElementById('ThemeBtn')
 
 const THEME_VALUES = [
   {
