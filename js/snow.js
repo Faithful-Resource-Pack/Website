137bca59e61985c2cc3e9ef1bd0c1d3efcbcc2ba
Evorp <3vorpgaming@gmail.com>
2024-11-26 23:42:27 -0800

    simplify snow script (why was it done like this)

--- js/snow.js
+++ js/snow.js
@@ -7,7 +7,7 @@ snowCSS.href = "/css/snow.css";
 snowCSS.type = "text/css";
 document.getElementsByTagName("head")[0].appendChild(snowCSS);
 
-// Array to store our Snowflake objects
+/** @type {Snowflake[]} Array to store our Snowflake objects */
 const snowflakes = [];
 
 // Global variables to store our browser's window size
@@ -20,9 +20,7 @@ const numberOfSnowflakes = 40;
 // Flag to reset the position of the snowflakes
 let resetPosition = false;
 
-/**
- * Constructor for our Snowflake object
- */
+/** Represents a single snowflake object */
 class Snowflake {
 	constructor(element, speed, xPos, yPos) {
 		// set initial snowflake properties
@@ -37,9 +35,7 @@ class Snowflake {
 		// setting an initial opacity and size for our snowflake
 		this.element.style.opacity = (0.1 + Math.random()) / 3;
 	}
-	//
-	// The function responsible for actually moving our snowflake
-	//
+	/** The function responsible for actually moving our snowflake */
 	update() {
 		// using some trigonometry to determine our x and y position
 		this.counter += this.speed / 5000;
@@ -47,17 +43,14 @@ class Snowflake {
 		this.yPos += Math.sin(this.counter) / 40 + this.speed / 30;
 		this.scale = 0.5 + Math.abs((10 * Math.cos(this.counter)) / 20);
 		// setting our snowflake's position
-		setTransform(Math.round(this.xPos), Math.round(this.yPos), this.scale, this.element);
+		this.setTransform(Math.round(this.xPos), Math.round(this.yPos), this.scale, this.element);
 		// if snowflake goes below the browser window, move it back to the top
 		if (this.yPos > browserHeight) this.yPos = -50;
 	}
-}
-
-/**
- * A performant way to set your snowflake's position and size
- */
-function setTransform(xPos, yPos, scale, el) {
-	el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0) scale(${scale}, ${scale})`;
+	/** A performant way to set your snowflake's position and size */
+	setTransform(xPos, yPos) {
+		this.element.style.transform = `translate3d(${xPos}px, ${yPos}px, 0) scale(${this.scale}, ${this.scale})`;
+	}
 }
 
 /**
@@ -66,13 +59,12 @@ function setTransform(xPos, yPos, scale, el) {
 function generateSnowflakes() {
 	// get our snowflake element from the DOM and store it
 
-	const originalSnowflake0 = document.querySelector(".snowflake0");
-	const originalSnowflake1 = document.querySelector(".snowflake1");
-	const originalSnowflake2 = document.querySelector(".snowflake2");
-	const originalSnowflake3 = document.querySelector(".snowflake3");
+	const originalSnowflakes = [".snowflake0", ".snowflake1", ".snowflake2", ".snowflake3"].map(
+		(sel) => document.querySelector(sel),
+	);
 
 	// access our snowflake element's parent container
-	const snowflakeContainer = originalSnowflake0.parentNode; // could be originalSnowflake0/1/2
+	const snowflakeContainer = originalSnowflakes[0].parentNode; // doesn't have to be zero
 	snowflakeContainer.style.display = "block";
 
 	// get our browser's size
@@ -80,44 +72,21 @@ function generateSnowflakes() {
 	browserHeight = document.documentElement.clientHeight;
 
 	// create each individual snowflake
-	for (let i = 0; i < numberOfSnowflakes / 4; i++) {
-		// clone our original snowflake and add it to snowflakeContainer
-		const snowflakeClone0 = originalSnowflake0.cloneNode(true);
-		snowflakeContainer.appendChild(snowflakeClone0);
-
-		const snowflakeClone1 = originalSnowflake1.cloneNode(true);
-		snowflakeContainer.appendChild(snowflakeClone1);
-
-		const snowflakeClone2 = originalSnowflake2.cloneNode(true);
-		snowflakeContainer.appendChild(snowflakeClone2);
-
-		const snowflakeClone3 = originalSnowflake3.cloneNode(true);
-		snowflakeContainer.appendChild(snowflakeClone3);
-
+	for (let i = 0; i < numberOfSnowflakes / originalSnowflakes.length; ++i) {
 		// set our snowflake's initial position and related properties
 		const initialXPos = getPosition(50, browserWidth);
 		const initialYPos = getPosition(50, browserHeight);
 		const speed = 5 + Math.random() * 40;
-		// create our Snowflake object
-
-		const snowflakeObject0 = new Snowflake(snowflakeClone0, speed, initialXPos, initialYPos);
-		snowflakes.push(snowflakeObject0);
-
-		const snowflakeObject1 = new Snowflake(snowflakeClone1, speed, initialXPos, initialYPos);
-		snowflakes.push(snowflakeObject1);
-
-		const snowflakeObject2 = new Snowflake(snowflakeClone2, speed, initialXPos, initialYPos);
-		snowflakes.push(snowflakeObject2);
-
-		const snowflakeObject3 = new Snowflake(snowflakeClone3, speed, initialXPos, initialYPos);
-		snowflakes.push(snowflakeObject3);
+		for (const snowflake of originalSnowflakes) {
+			// clone our original snowflake and add it to snowflakeContainer
+			const cloned = snowflake.cloneNode(true);
+			snowflakeContainer.appendChild(cloned);
+			snowflakes.push(new Snowflake(cloned, speed, initialXPos, initialYPos));
+		}
 	}
 
 	// remove the original snowflake because we no longer need it visible
-	snowflakeContainer.removeChild(originalSnowflake0);
-	snowflakeContainer.removeChild(originalSnowflake1);
-	snowflakeContainer.removeChild(originalSnowflake2);
-	snowflakeContainer.removeChild(originalSnowflake3);
+	originalSnowflakes.forEach((snowflake) => snowflakeContainer.removeChild(snowflake));
 	moveSnowflakes();
 }
 
