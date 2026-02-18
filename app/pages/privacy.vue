1c6cd0d6f17b2fafa0f2df813be99125e04ae37d
Evorp <75297863+3vorp@users.noreply.github.com>
2026-02-17 21:41:00 -0500

    feat: consistently use cards for bodies of text

--- app/pages/privacy.vue
+++ app/pages/privacy.vue
@@ -8,38 +8,40 @@ definePageMeta({
 	<h1 class="title mt-5 text-center">Privacy Policy</h1>
 	<h3 class="text-center mb-5">Last edited: 26th November 2024</h3>
 
-	<p>
-		This privacy policy describes how your personal information is collected, used and shared when
-		you visit <nuxt-link to="https://faithfulpack.net">https://faithfulpack.net</nuxt-link>.
-	</p>
+	<div class="card card-body">
+		<p>
+			This privacy policy describes how your personal information is collected, used and shared when
+			you visit <nuxt-link to="https://faithfulpack.net">https://faithfulpack.net</nuxt-link>.
+		</p>
 
-	<h2>Personal information we collect:</h2>
-	<p>None.</p>
+		<h2>Personal information we collect:</h2>
+		<p>None.</p>
 
-	<h2>Third-party services:</h2>
-	<p>
-		We use Cloudflare to make this website run. Please refer to their privacy policy for more
-		information:
-		<nuxt-link to="https://www.cloudflare.com/en-gb/gdpr/introduction">
-			https://www.cloudflare.com/en-gb/gdpr/introduction.
-		</nuxt-link>
-	</p>
+		<h2>Third-party services:</h2>
+		<p>
+			We use Cloudflare to make this website run. Please refer to their privacy policy for more
+			information:
+			<nuxt-link to="https://www.cloudflare.com/en-gb/gdpr/introduction">
+				https://www.cloudflare.com/en-gb/gdpr/introduction.
+			</nuxt-link>
+		</p>
 
-	<h2>Cookies:</h2>
-	<p>
-		We use a technology similar to cookies called "localstorage" to save your preferred website
-		theme and favorited add-ons.
-	</p>
+		<h2>Cookies:</h2>
+		<p>
+			We use a technology similar to cookies called "localstorage" to save your preferred website
+			theme and favorited add-ons.
+		</p>
 
-	<h2>Changes:</h2>
-	<p>
-		We may update this privacy policy from time to time in order to reflect changes to our practices
-		or for other operational, legal, or regulatory reasons.
-	</p>
+		<h2>Changes:</h2>
+		<p>
+			We may update this privacy policy from time to time in order to reflect changes to our
+			practices or for other operational, legal, or regulatory reasons.
+		</p>
 
-	<h2>Contact:</h2>
-	<p>
-		If you have any questions about this privacy policy, contact us by email at
-		contact@faithfulpack.net.
-	</p>
+		<h2>Contact:</h2>
+		<p>
+			If you have any questions about this privacy policy, contact us by email at
+			contact@faithfulpack.net.
+		</p>
+	</div>
 </template>
