417c61e412f64bf90cb63e41302ee791fdad64b6
Evorp <75297863+3vorp@users.noreply.github.com>
2026-06-21 14:45:08 -0700

    feat: new privacy policy

--- app/pages/privacy.vue
+++ app/pages/privacy.vue
@@ -7,42 +7,119 @@ definePageMeta({
 
 <template>
 	<h1 class="title mt-5 text-center">Privacy Policy</h1>
-	<p class="h3 text-center mb-5">Last updated Nov 26, 2024</p>
+	<p class="h3 text-center mb-5">Last updated Jun 21, 2026</p>
 
 	<div class="card card-body body-text">
 		<p>
-			This privacy policy describes how your personal information is collected, used and shared when
-			you visit <nuxt-link to="https://faithfulpack.net">https://faithfulpack.net</nuxt-link>.
+			This privacy policy describes how information is collected, used and shared when using
+			services operated by Faithful.
 		</p>
 
-		<h2>Personal information we collect:</h2>
-		<p>None.</p>
-
-		<h2>Third-party services:</h2>
+		<h2>Collected information:</h2>
 		<p>
-			We use Cloudflare to make this website run. Please refer to their privacy policy for more
-			information:
-			<nuxt-link to="https://www.cloudflare.com/en-gb/gdpr/introduction">
-				https://www.cloudflare.com/en-gb/gdpr/introduction.
-			</nuxt-link>
+			<b>
+				TL;DR: Faithful has not, does not, and will not collect personal information unless it is
+				explicitly given.
+			</b>
 		</p>
+		<ol>
+			<li>
+				Faithful user profiles can be customized by optionally providing a username, bio, Minecraft
+				UUID, and social media links. All of this information is voluntarily provided, can be
+				removed at any time, is stored locally, and will not be personally identifiable unless
+				personally identifiable information is provided.
+			</li>
+			<li>
+				By submitting an add-on for review, any information submitted with that add-on will be
+				stored for the purposes of being listed on the site. All add-on information and images are
+				voluntarily provided, can be removed at any time, are stored locally, and will not be
+				personally identifiable unless personally identifiable information is provided.
+			</li>
+			<li>
+				By submitting a texture on Discord, the image data and message content are stored in the
+				generated submission embed. Texture submissions can be removed at any time and are stored on
+				Discord's servers. All submission information is voluntarily provided and will not be
+				personally identifiable unless personally identifiable information is provided.
+				<br />
+				If a texture passes voting, a record of the submission and its authors is created.
+				Contribution history can be anonymized at the user's discretion.
+			</li>
+			<li>
+				A technology similar to cookies called "localstorage" is used to save preferred website
+				themes, favorited add-ons, and recent searches. On the Faithful Web Application, login
+				information and persistent user-customizable settings also use localstorage. Localstorage is
+				entirely stored on the user's device and cannot be read by Faithful or any other user.
+			</li>
+			<li>
+				A record of the number of times a Discord bot command has been used is stored on a local
+				database on the bot server. Faithful does not log messages, and these records are entirely
+				anonymous metrics.
+			</li>
+		</ol>
 
-		<h2>Cookies:</h2>
-		<p>
-			We use a technology similar to cookies called "localstorage" to save your preferred website
-			theme and favorited add-ons.
-		</p>
+		<h2>How information is used:</h2>
+		<ol>
+			<li>
+				Users can opt into having a public user page to display information about them, their
+				contributions, and their add-ons. This is entirely voluntary and can be disabled by
+				anonymizing one's user account. Additionally, user statistics are used to track project
+				growth and aggregate statistics.
+			</li>
+			<li>
+				Add-on information is used provide context for users interested in downloading the add-on,
+				aggregate statistics, and list them on the website accurately and correctly.
+			</li>
+			<li>
+				Texture submission information is used to provide context for submission voting, credit
+				co-authors accurately, and push texture files to the correct locations. Contributions are
+				tracked to aggregate statistics, prevent improper attribution, and preserve a historical
+				record of the packs' evolutions.
+			</li>
+			<li>
+				Localstorage data is used to remember user-configured settings between website visits and
+				prevent the web app logging out users between sessions.
+			</li>
+			<li>
+				Discord bot command records are completely anonymous usage metrics used to improve the bot.
+			</li>
+		</ol>
+		<h2>Opting out:</h2>
+		<ol>
+			<li>
+				A Faithful account and user profile can be deleted at any time using the profile page.
+			</li>
+			<li>
+				An add-on submission can be deleted at any time using the delete button on the add-on
+				submissions page.
+			</li>
+			<li>
+				A texture submission can be deleted at any time by using the reaction tray on the relevant
+				Discord submission embed.
+			</li>
+			<li>
+				Localstorage usage can be avoided by not setting website-specific settings or deleted by
+				clearing browser data.
+			</li>
+			<li>Adding to Discord bot records can be avoided by not interacting with bots.</li>
+		</ol>
 
-		<h2>Changes:</h2>
-		<p>
-			We may update this privacy policy from time to time in order to reflect changes to our
-			practices or for other operational, legal, or regulatory reasons.
-		</p>
-
-		<h2>Contact:</h2>
-		<p>
-			If you have any questions about this privacy policy, contact us by email at
-			contact@faithfulpack.net.
-		</p>
+		<h2>Finally:</h2>
+		<ol>
+			<li>
+				Faithful services may use or contain links to third-party services that have their own
+				privacy policies and terms of service. It is encouraged to review the privacy practices of
+				any third-party service before sharing personal information with them, as Faithful is not
+				responsible for the privacy practices, content, or data handling of these services.
+			</li>
+			<li>
+				Faithful does not provide services to children under the age of 13. If you are a parent or
+				guardian and you are aware that your child has provided Faithful with personal data, please
+				contact us to remove it.
+			</li>
+			<li>
+				We may update this privacy policy from time to time in order to reflect changes to our
+				practices or for other operational, legal, or regulatory reasons.
+			</li>
+		</ol>
 	</div>
 </template>
